5c1b6caf8a67c579feb5992b417f228b7dda8912
hugozepeda18 <a01228679@exatec.tec.mx>
2022-11-08 21:35:41 -0600

    [MAJOR]: Adding orders relationship to other tables and orders router

--- FASE_3/s7/routes/auth.js
+++ FASE_3/s7/routes/auth.js
@@ -1,24 +1,47 @@
 const express = require('express')
 const router = express.Router()
-const jwt = require('jsonwebtoken')
 const sequelize = require('../config/db')
+const jwt = require('jsonwebtoken')
 
-router.post('/login', (req, res) => {
-    const { body } = req
-    const user = await sequelize.models.users.findOne({
-                           where: body.email 
-                        })
-    if(!user) return res.status(401).json({ message: 'Unathorized'})
-    if(!user.validPassword(body.password)) return res.status(401).json({ message: 'Invalid credentials!'})
+router.post('/login', async (req, res) => {
+  const { body } = req
+  const user = await sequelize.models.users.findOne({ 
+                where: { email: body.email }
+              })
 
-    const token = jwt.sign({ userId: user.id}, 'secretKey', {
-        expiresIn: 3600
-    })
-    return res.json({ messages: 'Authenticated successfully!'})
+  if (!user) return res.status(401).json({ message: 'Unauthorized' })
+  if (!user.validPassword(body.password)) return res.status(401).json({ message: 'Invalid credentials!' })
+
+  const token = jwt.sign({ userId: user.id }, 'secretKey', {
+    expiresIn: 3600
+  })
+
+  return res.json({ message: 'Athenticated successfully!', token })
 })
 
-router.prototype('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+  const { body } = req;
+  let user = await sequelize.models.users.findOne({
+    email: body.email,
+  });
 
+  // Validation for known is the user's email exists
+  if (user) {
+    return res.status(400).json({ message: "this email is already registered" });
+  }
+
+  // Creating the user
+  user = await sequelize.models.users.create({
+    name: body.name,
+    lastname: body.lastname,
+    email: body.email,
+    password: body.password,
+    type: 'client',
+  })
+
+   // Saving user
+  await user.save();
+  return res.json({ message: 'Your account was created successfully'});
 })
 
 module.exports = router
